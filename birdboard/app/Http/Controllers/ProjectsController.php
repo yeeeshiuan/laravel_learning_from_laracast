4636a472cf00d364de46c149d9e38b7341cc9a22
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-27 20:17:56 +0800

    update the controller (small

--- birdboard/app/Http/Controllers/ProjectsController.php
+++ birdboard/app/Http/Controllers/ProjectsController.php
@@ -15,15 +15,10 @@ class ProjectsController extends Controller
 
     public function store()
     {
-		// volidate
 
-		request()->validate(['title' => 'required', 'description' => 'required']);
+		$attributes = request()->validate(['title' => 'required', 'description' => 'required']);
 
-		// persist
-
-		Project::create(request(['title', 'description']));
-
-		// redirect
+		Project::create($attributes);
 
 		return redirect('/projects');
     }
