ca5e901d7dde96be68bf73ea5283fc58f7f231b4
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-28 11:50:39 +0800

    update owner_id fetched by signing in

--- birdboard/app/Http/Controllers/ProjectsController.php
+++ birdboard/app/Http/Controllers/ProjectsController.php
@@ -23,9 +23,10 @@ class ProjectsController extends Controller
 
 		$attributes = request()->validate([
 			'title' => 'required', 
-			'description' => 'required',
-            'owner_id' => 'required'
+			'description' => 'required'
 		]);
+
+        $attributes['owner_id'] = auth()->id();
 
 		Project::create($attributes);
 
