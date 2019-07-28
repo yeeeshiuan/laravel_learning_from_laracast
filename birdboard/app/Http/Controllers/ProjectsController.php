d554307fe7bd2f471befb2568c9e3772ebc95db9
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-28 17:46:09 +0800

    Add Project Model test and Add project Model method

--- birdboard/app/Http/Controllers/ProjectsController.php
+++ birdboard/app/Http/Controllers/ProjectsController.php
@@ -15,7 +15,7 @@ class ProjectsController extends Controller
 
     public function show(Project $project) // auto-inject
     {
-        if ( auth()->id() != $project->owner_id )
+        if ( auth()->user()->isNot($project->owner) )
         {
             abort(403);
         }
