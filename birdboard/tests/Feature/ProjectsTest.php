2bcf6c15fd31b7b912e07bdd0e0dc24c01bd3d17
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-28 13:36:25 +0800

    update test with fake user to pass auth

--- birdboard/tests/Feature/ProjectsTest.php
+++ birdboard/tests/Feature/ProjectsTest.php
@@ -17,7 +17,12 @@ class ProjectsTest extends TestCase
 
         $this->withoutExceptionHandling();
 
-        $attributes = factory('App\Project')->raw();
+        $this->actingAs(factory('App\User')->create());
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
 
         $this->post('/projects', $attributes)->assertRedirect('/projects');
 
@@ -43,6 +48,8 @@ class ProjectsTest extends TestCase
     /** @test **/
     public function a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -51,16 +58,18 @@ class ProjectsTest extends TestCase
     /** @test **/
     public function a_project_requires_a_description()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 
     /** @test **/
-    public function a_project_requires_a_owner()
+    public function only_authenticated_user_can_create_projects()
     {
 
-        //$this->withoutExceptionHandling();
+        // $this->withoutExceptionHandling();
 
         $attributes = factory('App\Project')->raw();
 
