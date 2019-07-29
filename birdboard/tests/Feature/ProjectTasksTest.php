7fc526826bb50124340910e409bd669283186be5
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-29 14:49:21 +0800

    Add tasks feature test

---
+++ birdboard/tests/Feature/ProjectTasksTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Project;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectTasksTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test **/
+    public function a_project_can_have_tasks()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        $project = factory(Project::class)->create(['owner_id' => auth()->id()]);
+
+        $this->post($project->path() . '/tasks', ['body' => 'Test tasks']);
+
+        $this->get($project->path())
+                ->assertSee('Test task');
+
+    }
+}
