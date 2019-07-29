8e6115ac772b44bda83ffea145b033691a0d9cda
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-29 15:31:58 +0800

    Add task controller

---
+++ birdboard/app/Http/Controllers/ProjectTasksController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectTasksController extends Controller
+{
+    public function store(Project $project)
+    {
+
+    	$project->addTask(request('body'));
+
+    	return redirect($project->path());
+
+    }
+}
