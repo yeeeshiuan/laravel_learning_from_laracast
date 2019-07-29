73199bdfbf2e4b889911d8103ce2226f6bccffb8
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-29 16:56:37 +0800

    Add test only owner can add tasks to the project

--- birdboard/tests/Feature/ProjectTasksTest.php
+++ birdboard/tests/Feature/ProjectTasksTest.php
@@ -13,6 +13,22 @@ class ProjectTasksTest extends TestCase
     use RefreshDatabase;
 
     /** @test **/
+    public function only_the_owner_of_a_project_may_add_tasks()
+    {
+
+        $this->signIn();
+
+        $project = factory('App\Project')->create();
+
+        $this->post($project->path() . '/tasks', ['body' => 'Test task'])
+                ->assertStatus(403);
+
+        $this->assertDatabaseMissing('tasks', ['body' => 'Test task']);
+
+
+    }
+
+    /** @test **/
     public function a_project_can_have_tasks()
     {
 
