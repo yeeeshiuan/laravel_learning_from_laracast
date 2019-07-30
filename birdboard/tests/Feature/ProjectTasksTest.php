a7a27a540f06503402fca590237362b7b46a7c0b
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-30 22:44:01 +0800

    Add a test of Task to check the project owner is the right person

--- birdboard/tests/Feature/ProjectTasksTest.php
+++ birdboard/tests/Feature/ProjectTasksTest.php
@@ -29,6 +29,24 @@ class ProjectTasksTest extends TestCase
     }
 
     /** @test **/
+    public function only_the_owner_of_a_project_may_update_a_task()
+    {
+
+        $this->signIn();
+
+        $project = factory('App\Project')->create();
+
+        $task = $project->addTask('test task');
+
+        $this->patch($task->path(), ['body' => 'changed'])
+                ->assertStatus(403);
+
+        $this->assertDatabaseMissing('tasks', ['body' => 'changed']);
+
+
+    }
+
+    /** @test **/
     public function a_project_can_have_tasks()
     {
 
