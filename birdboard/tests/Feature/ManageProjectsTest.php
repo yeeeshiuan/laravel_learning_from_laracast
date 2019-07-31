276a75fe8b626114dd0ca2dc396a6e46d7989d11
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-31 12:46:21 +0800

    Add a test to the feature Project, that other user can't update the other project notes

--- birdboard/tests/Feature/ManageProjectsTest.php
+++ birdboard/tests/Feature/ManageProjectsTest.php
@@ -106,6 +106,18 @@ class ManageProjectsTest extends TestCase
     }
 
     /** @test **/
+    public function an_authenticated_user_cannot_update_the_projects_of_others()
+    {
+
+        $this->signIn();
+
+        $project = factory('App\Project')->create();
+
+        $this->patch($project->path(), [])->assertStatus(403);
+
+    }
+
+    /** @test **/
     public function a_project_requires_a_title()
     {
         $this->signIn();
