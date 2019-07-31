66a38058a6176ee1c5ab391cd297e8789b12454b
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-31 15:15:25 +0800

    update signIn method

--- birdboard/tests/TestCase.php
+++ birdboard/tests/TestCase.php
@@ -11,6 +11,12 @@ abstract class TestCase extends BaseTestCase
 
     protected function signIn($user = null)
     {
-    	$this->actingAs($user ?: factory('App\User')->create());
+
+    	$user = $user ?: factory('App\User')->create();
+
+    	$this->actingAs($user);
+
+    	return $user;
+    	
     }
 }
