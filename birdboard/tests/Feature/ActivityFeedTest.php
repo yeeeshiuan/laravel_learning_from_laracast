f718d6c25d2427dcfb4d169d5f780f745c66e83f
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-01 14:31:54 +0800

    Add a test

--- birdboard/tests/Feature/ActivityFeedTest.php
+++ birdboard/tests/Feature/ActivityFeedTest.php
@@ -3,20 +3,24 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ActivityFeedTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test **/
+    function creating_a_project_generates_activity()
+    {
+
+        $project = ProjectFactory::create();
+
+        $this->assertCount(1, $project->activity);
+
     }
+
+
 }
