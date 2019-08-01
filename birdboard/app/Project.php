675aae8d1a1362a67305b599bd5a23393c9f72b7
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-01 14:33:08 +0800

    update Project model to relate Activity model

--- birdboard/app/Project.php
+++ birdboard/app/Project.php
@@ -28,4 +28,9 @@ class Project extends Model
     {
     	return $this->tasks()->create(compact('body'));
     }
+
+    public function activity()
+    {
+        return $this->hasMany(Activity::class);
+    }
 }
