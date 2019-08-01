625a2c6e7287831bfb949ab1de7b82dba1040c5e
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-01 16:58:01 +0800

    update with seperate a test and add a not test

--- birdboard/tests/Feature/ProjectTasksTest.php
+++ birdboard/tests/Feature/ProjectTasksTest.php
@@ -74,6 +74,30 @@ class ProjectTasksTest extends TestCase
 
         $this->patch($project->tasks[0]->path(), [
 
+            'body' => 'changed'
+
+        ]);
+
+        $this->assertDatabaseHas('tasks', [
+
+            'body' => 'changed'
+
+        ]);
+
+
+    }
+
+    /** @test **/
+    public function a_task_can_be_completed()
+    {
+
+        $project = app(ProjectFactory::class)
+                    ->ownedBy($this->signIn())
+                    ->withTasks(1)
+                    ->create();
+
+        $this->patch($project->tasks[0]->path(), [
+
             'body' => 'changed',
 
             'completed' => true
@@ -85,6 +109,42 @@ class ProjectTasksTest extends TestCase
             'body' => 'changed',
 
             'completed' => true
+
+        ]);
+
+
+    }
+
+    /** @test **/
+    public function a_task_can_be_marked_as_incompleted()
+    {
+
+        $project = app(ProjectFactory::class)
+                    ->ownedBy($this->signIn())
+                    ->withTasks(1)
+                    ->create();
+
+        $this->patch($project->tasks[0]->path(), [
+
+            'body' => 'changed',
+
+            'completed' => true
+
+        ]);
+
+        $this->patch($project->tasks[0]->path(), [
+
+            'body' => 'changed',
+
+            'completed' => false
+
+        ]);
+
+        $this->assertDatabaseHas('tasks', [
+
+            'body' => 'changed',
+
+            'completed' => false
 
         ]);
 
