3b8fb0d46565b12568a7e635659a186c2f02a5ab
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-01 16:15:17 +0800

    Add two Task test for operating with Activities

--- birdboard/tests/Feature/ActivityFeedTest.php
+++ birdboard/tests/Feature/ActivityFeedTest.php
@@ -40,5 +40,43 @@ class ActivityFeedTest extends TestCase
 
     }
 
+    /** @test **/
+    function creating_a_new_task_records_project_activity()
+    {
+
+        $project = ProjectFactory::create();
+
+        $project->addTask('Some task');
+
+        $this->assertCount(2, $project->activity);
+
+        $this->assertEquals('created', $project->activity[0]->description);
+
+        $this->assertEquals('created_task', $project->activity[1]->description);
+
+    }
+
+    /** @test **/
+    function completing_a_task_records_project_activity()
+    {
+        $project = ProjectFactory::withTasks(1)->create();
+
+        $this->actingAs($project->owner)
+                ->patch($project->tasks[0]->path(), [
+
+                    'body' => 'foobar', 
+
+                    'completed' => true
+                ]);
+
+        $this->assertCount(3, $project->activity);
+
+        $this->assertEquals('created', $project->activity[0]->description);
+
+        $this->assertEquals('created_task', $project->activity[1]->description);
+
+        $this->assertEquals('completed_task', $project->activity[2]->description);
+    }
+
 
 }
