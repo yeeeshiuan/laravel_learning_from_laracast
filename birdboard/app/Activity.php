047cb2a42c185e337ca31b61b721ca5966fb36e5
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-02 20:59:21 +0800

    update Activity model with a new method (subject)

--- birdboard/app/Activity.php
+++ birdboard/app/Activity.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     protected $guarded = [];
+
+    public function subject()
+    {
+
+    	return $this->morphTo();
+
+    }
 }
