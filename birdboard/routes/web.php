9ced723f431add44ebc0fbef21d3be6aa9ffceb0
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-03 18:57:19 +0800

    simpify the routers

--- birdboard/routes/web.php
+++ birdboard/routes/web.php
@@ -17,19 +17,21 @@ Route::get('/', function () {
 
 Route::group(['middleware' => 'auth'], function () {
 
-	Route::get('/projects', 'ProjectsController@index');
+	// Route::get('/projects', 'ProjectsController@index');
 
-	Route::get('/projects/create', 'ProjectsController@create');
+	// Route::get('/projects/create', 'ProjectsController@create');
 
-	Route::get('/projects/{project}', 'ProjectsController@show');
+	// Route::get('/projects/{project}', 'ProjectsController@show');
 
-	Route::get('/projects/{project}/edit', 'ProjectsController@edit');
+	// Route::get('/projects/{project}/edit', 'ProjectsController@edit');
 
-	Route::patch('/projects/{project}', 'ProjectsController@update');
+	// Route::patch('/projects/{project}', 'ProjectsController@update');
 
-	Route::post('/projects', 'ProjectsController@store');
+	// Route::post('/projects', 'ProjectsController@store');
 
-	Route::delete('/projects/{project}', 'ProjectsController@destroy');
+	// Route::delete('/projects/{project}', 'ProjectsController@destroy');
+
+	Route::resource('projects', 'ProjectsController');
 
 
 	Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
