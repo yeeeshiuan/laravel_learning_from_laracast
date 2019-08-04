90fa1cb5530f96a0a77b3fd21471c2373ced8d98
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-04 14:44:10 +0800

    remove unused comments

--- birdboard/routes/web.php
+++ birdboard/routes/web.php
@@ -17,20 +17,6 @@ Route::get('/', function () {
 
 Route::group(['middleware' => 'auth'], function () {
 
-	// Route::get('/projects', 'ProjectsController@index');
-
-	// Route::get('/projects/create', 'ProjectsController@create');
-
-	// Route::get('/projects/{project}', 'ProjectsController@show');
-
-	// Route::get('/projects/{project}/edit', 'ProjectsController@edit');
-
-	// Route::patch('/projects/{project}', 'ProjectsController@update');
-
-	// Route::post('/projects', 'ProjectsController@store');
-
-	// Route::delete('/projects/{project}', 'ProjectsController@destroy');
-
 	Route::resource('projects', 'ProjectsController');
 
 
