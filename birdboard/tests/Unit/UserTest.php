098ad278b7d81fe94b986a871aa18f12ae664a68
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-04 17:16:20 +0800

    update User model unit test

--- birdboard/tests/Unit/UserTest.php
+++ birdboard/tests/Unit/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Database\Eloquent\Collection;
@@ -19,6 +20,30 @@ class UserTest extends TestCase
     	$user = factory('App\User')->create();
 
     	$this->assertInstanceOf(Collection::class, $user->projects);
+
+    }
+
+    /** @test **/
+    public function a_user_has_accessible_projects()
+    {
+
+    	$john = $this->signIn();
+
+    	ProjectFactory::ownedBy($john)->create();
+
+    	$this->assertCount(1, $john->accessibleProjects());
+
+    	$sally = factory(\App\User::class)->create();
+
+    	$nick = factory(\App\User::class)->create();
+
+    	ProjectFactory::ownedBy($sally)->create()->invite($nick);
+
+    	$this->assertCount(1, $john->accessibleProjects());
+
+    	ProjectFactory::ownedBy($sally)->create()->invite($john);
+
+    	$this->assertCount(2, $john->accessibleProjects());
 
     }
 }
