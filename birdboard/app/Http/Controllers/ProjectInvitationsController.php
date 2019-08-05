e9b2b2260a77f9a5254a7a95e9da016d15d53f68
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-05 11:00:56 +0800

    Add Project Invitations Controller

---
+++ birdboard/app/Http/Controllers/ProjectInvitationsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Project;
+
+class ProjectInvitationsController extends Controller
+{
+    
+	public function store(Project $project)
+	{
+
+		$this->authorize('update', $project);
+
+		request()->validate([
+
+			'email' => [ 'required', 'exists:users,email']
+
+		], [
+
+			'email.exists' => 'The user you are inviting must have a Birdboard account.'
+
+		]);
+
+		$user = User::whereEmail(request('email'))->first();
+
+		$project->invite($user);
+
+		return redirect($project->path());
+
+	}
+
+}
