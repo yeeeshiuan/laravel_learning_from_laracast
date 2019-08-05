a4052fb8ab4be0e12e39fb70a1d2b16c2547c2ea
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-05 11:00:07 +0800

    Add a new router

--- birdboard/routes/web.php
+++ birdboard/routes/web.php
@@ -25,6 +25,9 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::patch('/projects/{project}/tasks/{task}', 'ProjectTasksController@update');
 
 
+	Route::post('/projects/{project}/invitations', 'ProjectInvitationsController@store');
+
+
 	Route::get('/home', 'HomeController@index')->name('home');
 });
 
