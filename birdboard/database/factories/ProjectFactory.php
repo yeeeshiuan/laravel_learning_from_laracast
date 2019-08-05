442ebc1baed8e346b5c02b31ac52914373bce34c
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-08-05 14:55:57 +0800

    update the factory

--- birdboard/database/factories/ProjectFactory.php
+++ birdboard/database/factories/ProjectFactory.php
@@ -9,6 +9,7 @@ $factory->define(Project::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(4),
         'description' => $faker->sentence(4),
+        'notes' => 'projects notes',
         'owner_id' => factory('App\User'::class)
     ];
 });
